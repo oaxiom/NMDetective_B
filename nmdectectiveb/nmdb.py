0a5bc910885c2a1e90da04f6e50a5162f643ad07
oaxiom <oaxiom@gmail.com>
2023-05-30 11:36:17 +0800

    first theoretical working version

--- nmdectectiveb/nmdb.py
+++ nmdectectiveb/nmdb.py
@@ -1,6 +1,8 @@
 
 import sys, os, gzip
 from shlex import split as shlexsplit
+from collections import Counter
+import matplotlib.pyplot as plot
 
 '''
 
@@ -15,13 +17,17 @@ This version is generalised for GTFs, and standalone commandline
 '''
 
 class NMDB:
-    def __init__(self):
-        pass
+    def __init__(self, log=None):
+        assert log, 'You must provide a log handle'
+        self.log = log
 
     def gtf_parser(self, filename, gziped=False):
         '''
         Simple parser for GFT
         '''
+
+        if filename.endswith('.gz'):
+            gziped = True
 
         if gziped:
             gtfoh = gzip.open(filename, "rt")
@@ -53,115 +59,192 @@ class NMDB:
         return
 
 
-    def NMDetective_B_score(inlastexon, orflength, exonlength, within_50nt_of_lastEJ):
+    def NMDetective_B_score(self, inlastexon, orflength, exonlength, within_50nt_of_lastEJ):
         '''
 
         Scoring decision tree
 
         '''
         if inlastexon:
-            return 0.0, 'Last exon'
+            return 0.0, 'Last exon\n(Score=0.0)'
 
         if orflength < 150: # Distance from STOP to START
-            return 0.12, 'Start-proximal'
+            return 0.12, 'Start-proximal\n(Score=0.12)'
 
         if exonlength >407: # exon length the STOP codon is in
-            return 0.41, 'Long exon'
+            return 0.41, 'Long exon\n(Score=0.41)'
 
         if within_50nt_of_lastEJ: # EJC will block NMD if STOP within 50 nt.
-            return 0.2, '50 nt rule'
+            return 0.2, '50 nt rule\n(Score=0.20)'
 
-        return 0.65, 'Trigger NMD'
+        return 0.65, 'Trigger NMD\n(Score=0.65)'
 
     def score(self, gtf_filename):
 
-        res = {
-            'MS Hit': [],
-            'No MS hit': [],
-            'Variant': [],
-            'Matching': [],
-            }
+        self.log.info('Starting score')
 
-        data = {
-            'Matching': matching_coding,
-            'Variant': variant_coding,
-            'MS Hit': has_peptide_hit,
-            'No MS hit': no_peptide_hit,
-            }
-
-        __notfound = 0
-        __nostop = 0
+        scores = []
+        cats = []
 
         # First make bundles for each transcript
         transcript_bundles = {}
-        for item in gtf_parser()
+        for idx, item in enumerate(self.gtf_parser(gtf_filename)):
+            if idx > 0 and idx % 1e4 == 0:
+                self.log.info(f'{idx:,} done')
+                break
 
-        for k in data:
-            for transcript in data[k]:
-                strand = transcript['strand']
+            if item['gene_type'] != 'protein_coding':
+                continue
 
-                cds_key_to_use = None
-                if 'cds_loc' in transcript and transcript['cds_loc']:
-                    cds_key_to_use = 'cds_loc'
-                elif 'cds_genome_loc' in transcript and transcript['cds_genome_loc']:
-                    cds_key_to_use = 'cds_genome_loc'
-                else:
-                    cds_key_to_use = 'cds_local_to_genome'
+            if item['feature'] == 'gene': # No useful data here
+                continue
 
-                orflength = abs(transcript[cds_key_to_use]['right'] - transcript[cds_key_to_use]['left'])
+            if item['transcript_id'] not in transcript_bundles:
+                transcript_bundles[item['transcript_id']] = []
 
-                if transcript[cds_key_to_use]['right'] == 0:
-                    # Ignore misannotated GENCODE CDSs
-                    continue
+            transcript_bundles[item['transcript_id']].append(item)
 
+        self.log.info(f'Processed {idx:,} entries in the GTF')
+        self.log.info(f'Found {len(transcript_bundles):,} transcripts')
+
+        for transcript_id, transcript in transcript_bundles.items():
+            exonStarts = []
+            exonEnds   = []
+            # I need to convert the transcript bundle into:
+            chrom = transcript[0]['chrom']
+            strand = transcript[0]['strand']
+            START = 0
+            STOP = 0
+
+            for entry in transcript:
+                '''
+                if entry['feature'] == 'transcript':
+                    if strand == '+':
+                        tss = entry['left']
+                        tts = entry['right']
+                    else:
+                        tss = entry['right']
+                        tts = entry['left']
+                '''
+
+                if entry['feature'] == 'start_codon': # This is not quite right as the codon is 3 bp. However, I think for these purposes it's close enough
+                    START = entry['left']
+                elif entry['feature'] == 'stop_codon':
+                    STOP = entry['left']
+
+                elif entry['feature'] == 'exon':
+                    if strand == '+':
+                        exonStarts.append(entry['left'])
+                        exonEnds.append(entry['right'])
+                    else:
+                        exonStarts.append(entry['right'])
+                        exonEnds.append(entry['left'])
+
+            # I need to wqork out the orflength
+            orflength = 0 # currently wrong...
+            for exon_num, exons in enumerate(zip(exonStarts, exonEnds)):
                 if strand == '+':
-                    STOP = transcript[cds_key_to_use]['right']
+                    if START >= exons[0] and START <= exons[1] and STOP >= exons[0] and STOP <= exons[1]:
+                        # START and STOP are in the same exon
+                        orflength += STOP - START
+
+                    elif START >= exons[0] and START <= exons[1]:
+                        # START is inside this exon
+                        orflength +=  exons[1] - START
+
+                    elif STOP >= exons[0] and STOP <= exons[1]:
+                        # STOP is inside this exon
+                        orflength += STOP - exons[0]
+
+                    else:
+                        # check it's not a UTR
+                        if exons[0] >= START and exons[1] <= STOP:
+                            orflength += exons[1] - exons[0]
+
                 else:
-                    STOP = transcript[cds_key_to_use]['left']
+                    if START >= exons[1] and START <= exons[0] and STOP >= exons[1] and STOP <= exons[0]:
+                        # START and STOP are in the same exon
+                        orflength += START - STOP
 
-                inlastexon = False
-                within_50nt_of_lastEJ = False
+                    elif START >= exons[1] and START <= exons[0]:
+                        # START is inside this exon
+                        orflength += START - exons[1]
 
-                if STOP == 0:
-                    __nostop += 1
-                    continue
+                    elif STOP >= exons[1] and STOP <= exons[0]:
+                        # STOP is inside this exon
+                        orflength +=  exons[0] - STOP
 
-                #print(strand, list(zip(transcript['exonStarts'], transcript['exonEnds'])))
-                for exon_num, exons in enumerate(zip(transcript['exonStarts'], transcript['exonEnds'])):
-                    if STOP >= exons[0] and STOP <= exons[1]:
-                        if strand == '+':
-                            d = exons[1] - STOP
-                            if exon_num == len(transcript['exonEnds'])-1:
-                                inlastexon = True
-                                if d < 50:
-                                    within_50nt_of_lastEJ = True
+                    else:
+                        # check it's not a UTR
+                        if exons[0] <= START and exons[1] >= STOP:
+                            orflength += exons[0] - exons[1]
+#0 21651836
+#1 21651779
+            if START == 0 or STOP == 0:
+                log.info(f'{transcript_id} has no start_codon or stop_codon key in the GTF, skipping')
+                continue
 
-                        else:
-                            d = STOP - exons[0]
-                            if exon_num == 0:
-                                inlastexon = True
-                                if d < 50:
-                                    within_50nt_of_lastEJ = True
+            print(transcript_id, strand, START, STOP, exonStarts, exonEnds)
+            print(orflength)
 
-                        exonlength = exons[1] - exons[0]
+            inlastexon = False
+            within_50nt_of_lastEJ = False
 
-                nmd_score, nmd_class = NMDetective_B_score(inlastexon, orflength, exonlength, within_50nt_of_lastEJ)
+            for exon_num, exons in enumerate(zip(exonStarts, exonEnds)):
+                if STOP >= exons[0] and STOP <= exons[1]:
+                    if strand == '+':
+                        d = exons[1] - STOP
+                        if exon_num == len(exonEnds)-1:
+                            inlastexon = True
+                        if d < 50:
+                            within_50nt_of_lastEJ = True
 
-                res[k].append(nmd_score)
+                    else:
+                        d = STOP - exons[0]
+                        if exon_num == 0:
+                            inlastexon = True
+                        if d < 50:
+                            within_50nt_of_lastEJ = True
 
-        return res
+                    exonlength = exons[1] - exons[0]
+
+            nmd_score, nmd_class = self.NMDetective_B_score(inlastexon, orflength, exonlength, within_50nt_of_lastEJ)
+
+            cats.append(nmd_class)
+            scores.append(nmd_score)
+
+        self.scores = scores
+        self.cats = cats
+
+        return scores, cats
 
     def plots(self, label):
         '''
         Output a few plots
 
         '''
-
         # Pie chart;
+        pie_summ = Counter(self.cats)
+        fig = plot.figure(figsize=[3,3])
+        ax = fig.add_subplot(111)
+        ax.pie(pie_summ.values(), labels=pie_summ.keys(), autopct='%1.1f%%',
+            textprops={'fontsize': 6})
+        fig.savefig(f'{label}.pie.pdf')
+
 
 if __name__ == '__main__':
-    n = NMDB()
+    import logging
 
-    res = n.score('../test/test_gtf.gtf.gz')
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)-8s: %(message)s',
+                    datefmt='%m-%d %H:%M')
 
-    print(res)
+    log = logging.getLogger('nmdetect')
+    log.setLevel(logging.INFO)
+
+    mpl_logger = logging.getLogger('matplotlib') # Bodge to silence the matplotlib logging
+    mpl_logger.setLevel(logging.WARNING)
+
+    n = NMDB(log=log)
+    n.score('../test/test_gtf.gtf.gz')
+    n.plots('../test/test')
